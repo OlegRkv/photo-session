9be8b7fda10b4b23cd6f2a89274873c30e856c65
OlegRkv <oleg-rkv@yandex.ru>
2021-11-20 17:07:30 +0400

    add section reviews

--- gulpfile.js
+++ gulpfile.js
@@ -46,6 +46,7 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/@fancyapps/ui/dist/fancybox.umd.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
